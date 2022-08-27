645a1b85338f75a0e2635786b60a4a18aad921e8
junseok yun <roadvirushn@gmail.com>
2022-08-27 23:56:14 +0900

    [FIX]Fix capture group variable erros and Better matching Regex
    1. regex nil or empty string problem resolved.\n 2.Fix regex for better matching

--- docs/_plugins/obsidian/preprocesses/PreprocessImageLink.rb
+++ docs/_plugins/obsidian/preprocesses/PreprocessImageLink.rb
@@ -1,37 +1,29 @@
 module PreprocessImageLink 
     OBSIDIAN_IMAGE_LINK_REGEX = /!\[\[(?:(?!https?:\/\/)(?:.*\/)?([^\[\]\|]+\.[a-z]{3,4}))??(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9(@:%_\+.~#?&\/=]*))?(?:\|(.*))?\]\](?=[^`]*(?:`[^`]*`[^`]*)*$)/
-    MARKDOWN_IMAGE_LINK_REGEX =  /!\[(?:([^\[\]\|]*))?\]\((?:(?!https?:\/\/)(?:.*\/)?([^\[\]\|]+\.[a-z]{3,4}))?(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9(@:%_\+.~#?&\/=]*))?\)(?=[^`]*(?:`[^`]*`[^`]*)*$)/
+    MARKDOWN_IMAGE_LINK_REGEX =  /!\[(?:([^\[\]\|]+))?\]\((?:(?!https?:\/\/)(?:.*\/)?([^\[\]\|]+\.[a-z]{3,4}))?(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9(@:%_\+.~#?&\/=]*))?\)(?=[^`]*(?:`[^`]*`[^`]*)*$)/
 
     def build_markdown_img(linkData, static_dir) 
 
-        staticImgDir = static_dir + '/' + (linkData['staticImgfile'] || '')
+        staticImgDir = static_dir + '/' + (linkData[:staticImgFile] || '')
 
-        src = linkData['externalURL'] || staticImgDir
+        src = linkData[:externalURL] || staticImgDir
 
-        return '![%s](%s)' % [linkData['altText'],src] 
+        return '![%s](%s)' % [linkData[:altText], src] 
     end
+
     def convert_image_link(post)  
         static_dir = '/' + Jekyll.configuration({})['static_img_dir'] + '/' + post.date.strftime("%Y-%m-%d") + '-' + post['slug'] 
         return post.content
         .gsub(MARKDOWN_IMAGE_LINK_REGEX){ |matched|
-            if $2 == "" || $2 == nil
-               staticImgfile = nil 
-            else
-                # Don't touch capture group value directly like this => ($2&.gsub("%20", ' '))
-                # It will cause misterious result to other capture group values like $1, $3 etc...
-                staticImgfile = $2 
-                staticImgfile = staticImgfile.gsub('%20', ' ')
-            end
-
             matched = build_markdown_img({
                 altText: $1,
-                staticImgfile: staticImgfile,
+                staticImgFile: $2&.gsub('%20', ' '),
                 externalURL: $3
             }, static_dir)
         }
         .gsub(OBSIDIAN_IMAGE_LINK_REGEX){ |matched|
             matched = build_markdown_img({
-                staticImgfile: $1,
+                staticImgFile: $1,
                 externalURL: $2,
                 altText: $3
             }, static_dir)
--- docs/_plugins/obsidian/preprocesses/PreprocessWikiLink.rb
+++ docs/_plugins/obsidian/preprocesses/PreprocessWikiLink.rb
@@ -1,23 +1,13 @@
 module PreprocessWikiLink
-    OBSIDIAN_LINK_REGEX = /(?<!!)\[\[((?!https?:\/\/)[^\]\|\#]+)?(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9(@:%_\+.~#?&\/=]*))?([^\]\|\)]*(\#[^\#\]\|]+))?(?:\|([^\#\|\]]*))?\]\](?=[^`]*(?:`[^`]*`[^`]*)*$)/
-    MARKDOWN_LINK_REGEX = /(?<!!)\[([^\#\|\]]+)?\]\(((?!https?:\/\/)[^\]\|\#]*)?(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[\-a-zA-Z0-9(@\:%_\+.~\#?&\/=]*))?([^\]\|\)]*(\#[^\#\]\|]+))?\)(?=[^`]*(?:`[^`]*`[^`]*)*$)/
+    OBSIDIAN_LINK_REGEX = /(?<!!)\[\[((?:(?!https?:\/\/).*\/)?(?:([^\|\/\^\#\*\?\]\[]+)))?((?:(?:\#[^#\]\[\)\|]*(?![\|\]]))*(\#[^\)\]\|]*)?)?)?(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[\-a-zA-Z0-9(@\:%_\+.~\#?&\/=]*))?(?:\|([^\#\|\]\[]+))?\]\](?=[^`]*(?:`[^`]*`[^`]*)*$)/
+    MARKDOWN_LINK_REGEX = /(?<!!)\[([^\#\|\]]+)?\]\(((?:(?!https?:\/\/).*\/)?(?:([^\|\/\^\#\*\?\]\[]+))?(?:((?:\#[^#\`\)\(]+)+(?!\)))?(\#[^#\`\)\(]+)?)?)?(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[\-a-zA-Z0-9(@\:%_\+.~\#?&\/=]*))?\)(?=[^`]*(?:`[^`]*`[^`]*)*$)/
 
-    def convert_wiki_link(site, post)  
+    def convert_wiki_link(site, post)
         return post.content
-        .gsub(MARKDOWN_LINK_REGEX){ |matched|
-            # why the empty string are coming out from unmatched results?
-            if $2 == "" || $2 == nil
-               postName = nil 
-            else
-                # Don't touch capture group value directly like this => ($2&.gsub("%20", ' '))
-                # It will cause misterious result to other capture group values like $4, $5 etc...
-                postName = $2 
-                postName = postName.gsub('%20', ' ')
-            end
-            
+        .gsub(MARKDOWN_LINK_REGEX){ |matched|            
             linkData = { 
                 altText: $1,
-                postName:postName,
+                postName: $2&.gsub('%20', ' '),
                 externalURL: $3,
                 rawHeadings: $4,
                 targetHeading: $5
@@ -39,7 +29,7 @@ module PreprocessWikiLink
         # Destructuring Hash to each variables.
         altText, externalURL, postName, rawHeadings, targetHeading = 
             linkData.values_at(:altText, :externalURL, :postName, :rawHeadings, :targetHeading)
-        puts linkData
+
         # Set innerText of <a> tag by priorities.
         innerText = \
             altText \
@@ -47,12 +37,13 @@ module PreprocessWikiLink
             || raw_headings_to_innertext((postName || '') + (rawHeadings || '')) \
             || 'Empty Link'
         isInternalLink = postName ? post['title'] == postName : (targetHeading || externalURL&.match(post.url))
+
         # Set href property of <a> tag. Check the outlink post name, then Link to desired headings.
         href = \
             externalURL \
             || (isInternalLink \
                 ? (raw_headings_to_href(rawHeadings) || "#") \
-                : link_to_other_post(site.posts,postName,targetHeading))
+                : link_to_other_post(site.posts, postName, targetHeading))
             
         return '[%s](%s)' % [innerText || "🔗", href]    
     end
