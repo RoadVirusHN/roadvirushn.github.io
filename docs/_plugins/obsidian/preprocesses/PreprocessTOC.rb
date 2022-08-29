fad2eaa448bcbd58da98117ebb4c7ca931fc48eb
junseok yun <roadvirushn@gmail.com>
2022-08-29 11:48:30 +0900

    [FEAT] Add custom headings id and wikilink system

--- docs/_plugins/obsidian/preprocesses/PreprocessTOC.rb
+++ docs/_plugins/obsidian/preprocesses/PreprocessTOC.rb
@@ -1,9 +1,17 @@
 module PreprocessTOC
-    OBSIDIAN_TOC_REGEX = /\[TOC\]|```toc[^`]*```/i
-
+    OBSIDIAN_TOC_REGEX = /(?:\[TOC\]|```toc[^`]*```)(?=[^`]*(?:`[^`]*`[^`]*)*\Z)/i
+    MARKDOWN_HEADINGS = /(^#+[^#\n]+(?=[^`]*(?:`[^`]*`[^`]*)*\Z))/
     def convert_TOC(str)
-        return str.gsub(OBSIDIAN_TOC_REGEX) { |matched|
+        return generate_headings_id(str.gsub(OBSIDIAN_TOC_REGEX) { |matched|
             matched = "* TOC\n{:toc}"
+        })
+    end
+
+    def generate_headings_id(str)
+        return str.gsub(MARKDOWN_HEADINGS) { |matched| 
+            headings = $1
+            custom_id = headings.downcase.gsub(/^#+ +/, '').gsub(/[!@$%^&*()_+\-=\[\]{};\':\"\\|,.<>\/? ]+$/, '')&.gsub(/^[!@$%^&*()_+\-=\[\]{};\':\"\\|,.<>\/? ]+/, '')&.gsub(/[!@$%^&*()_+\-=\[\]{};\':\"\\|,.<>\/? ]+/, '-')
+            matched = "#{headings}\n{: ##{custom_id}}"
         }
     end
 end
